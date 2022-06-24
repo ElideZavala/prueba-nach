13b7aa557de634da507e6c8738b2da3eb4d93095
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-24 16:49:31 -0500

    creacion de la tabla de empleados

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,56 +1,59 @@
 import { useEffect, useState } from "react";
 import getEmployees from "../helpers/getEmployees"
 import moment from "moment";
+import MUIDataTable from "mui-datatables";
+
+const options = {filterType: 'checkbox',};
 
 const Employees = () => {
-
 	const [ employees, setemployees] = useState([]);
-	const [success, setSuccess] = useState(false)
 	const [ error, setError ] = useState(null);
 
 	useEffect(() => {
 		updateEmployees();
 	}, [])
-
+	
 	/* ******* llamado a la API de Empleados ******* */
 	const updateEmployees = () => {
 		getEmployees()
-			.then((NewEmployees) => {
-				setemployees(NewEmployees.data.employees)
-				setSuccess(NewEmployees.success)
+		.then((NewEmployees) => {
+			const employees = NewEmployees.data.employees
+			setemployees(employees)
 			})
-			.catch((error) => {
-				console.log(error)
-				setError("Error al cargar los usuarios")
+		.catch((error) => {
+			console.log(error)
+			// setError("Error al cargar los usuarios")
       })
 	}
-
 	
+	const columns = [
+		{
+			name: "id",
+			label: "ID"
+		},
+		{
+			name: "last_name",
+			label: "lAST NAME"
+		},
+		{
+			name: "name",
+			label: "NAME"
+		},
+		{
+			name:  "bithday",
+			label: "BIRTHDAY"
+		}
+	];
 
-	
+
   return (
-
-	 <div className="contain__table">
-		<table className='contain__table--employees'>
-			<thead className='contain__table--employees__column'>
-				<tr>
-					<th>No.</th>
-					<th>Name</th>
-					<th>Last Name</th>
-					<th>Birthday</th>
-				</tr>
-			</thead>
-			<tbody className='contain__table--row'>
-				{employees.map((employee, i) => (
-					<tr key={employee.id}>
-						<td>{i + 1}</td>
-						<td>{employee.last_name}</td>
-						<td>{employee.name}</td>
-						<td>{moment(employee.birthday).format("MMM DD YY") }</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+	  <div className="contain__table">
+		 <MUIDataTable
+			 title={"Lista de empleados"}
+			 data={employees}
+			 columns={columns}
+			 options={options}
+		 />
 	 </div>
   )
 }
