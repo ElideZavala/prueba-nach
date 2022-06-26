3d21705e0c5840843133036b30144fc28c51d583
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-25 21:36:12 -0500

    modificamos el store de redux por errores y el DataProvider

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,12 @@ import { createStore, applyMiddleware } from "redux";
 // import { configureStore } from "@reduxjs/toolkitx";
 import thunk  from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import rootReducer from './reducers/index';
 import { Provider } from "react-redux";
 
 const store = createStore(
 	rootReducer,
-	composeWithDevTools(applyMiddleware(thunk))
+	composeWithDevTools(applyMiddleware( thunk))
 )
 
 const DataProvider = ({children}) => {
