daa2164a9f1d102c353728a75b1aceb0bedf8a34
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-25 21:38:53 -0500

    creacion del componente Slider

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,50 +1,55 @@
-import { useRef, useState, useEffect } from "react"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch  } from "react-redux";
+import Sliders from "../componets/Sliders";
+
+import { addImage } from '../redux/actions/imageActions'
 
 const Upload = () => {
-	const [image, setImage] = useState('');
-	const [preview, setPreview] = useState('')
-	const fileInputRef = useRef('');
+	const dispatch = useDispatch();
+	const [preview, setPreview] = useState(null);
+	const [saveImage, setSaveImage] = useState("")
 
-	useEffect(() => {
-		if (image) {
-			const reader = new FileReader();
-			reader.onloadend = () => {
-				setPreview(reader.result)
-			}
-			reader.readAsDataURL(image);
-		} else {
-			setPreview(null)
+	const changeImage = (e) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(e.target.files[0])
+		reader.onload = (e) => {
+			e.preventDefault();
+			setPreview(e.target.result);
 		}
-	})
+	}
+	
+	const uploadImage = () => {
+		console.log({ id: Date.now(), image: preview})
+		const newImage = { id: Date.now(), image: preview}
+		dispatch(addImage(newImage));
+	}
 	
   return (
-	 <div className="upload">
-		<form className="upload__form">
+	<> 
+		<div className="upload">
+			<form className="upload__form">
+				<input 
+					className="upload__form--input"
+					type="file"
+					accept="image/*"
+					multiple
+					onChange={e => {
+						changeImage(e);
+					}}
+				/>
+				<div className="upload__form--text">
+					<h3> Drag and drop a file or select add multiple images</h3>
+				</div>
+				<div className="upload__form--showImage">
+					<img src={preview} alt="" height="150px" width="250px"/>
+				</div>
+
+			</form>
+			<Sliders preview={preview}/>
+			<button className="upload__form--button" onClick={uploadImage}>Save Image</button>
 			
-			{ preview 
-				? ( <img className="upload__form--img" src={preview} onClick={() => setImage(null)}/> )
-				: (<button className="upload__form--button" onClick={(e) => {
-						e.preventDefault();
-						fileInputRef.current.click();
-						}}>
-					add Image
-					</button> 
-					)}
-			<input 
-				className="upload__form--input" 
-				type="file" 
-				ref={fileInputRef}
-				accept="image/*"
-				onChange={(e) => {
-					const file = e.target.files[0];
-					if (file && file.type.substr(0, 5) === "image") {
-						setImage(file);
-					} else {
-						setImage(null);
-					}
-				}}/>
-		</form>
-	 </div>
+		</div>
+	</>
   )
 }
 
