c841c5a32ffe63b3c151d6671a9c1ce7f01418b0
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-27 16:29:09 -0500

    eliminamos archivo post

--- src/helpers/getEmployees.js
+++ src/helpers/getEmployees.js
@@ -1,5 +1,3 @@
-import { moment } from "moment";
-
 const getEmployees = async () => {
 	const url = 'https://6edeayi7ch.execute-api.us-east-1.amazonaws.com/v1/examen/employees/:elide';
 	const res = await fetch(url);
@@ -8,13 +6,9 @@ const getEmployees = async () => {
 		const { url, status, statusText } = res;
 		throw Error(`Error: ${status} ${statusText} in fetch ${url}`);
 	}
-
 	
 	const datas = await res.json(); 
-	const employees = datas.data.employees; 
-	// const birthday = allemployees.map(employee => moment(employee.birthday).format("MM Do YY"));
-	// const employees = {...allemployees, birthday}
-	// console.log(birthday)
+	const employees = await datas.data.employees; 
 	return employees;
 	
 }
